9684337ca25788b599d79088d0235fee200fc903
Chris Ward <wadetyler50@gmail.com>
2023-11-13 03:05:33 -0800

    commit

--- src/Components/Dashboard/dashboard.jsx
+++ src/Components/Dashboard/dashboard.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import TopNavigation from '../TopNavigation/TopNavigation'; // Updated path
+import TopNavigation from '../TopNavigation/TopNavigation';
 import './Dashboard.css';
+
+const teamMembers = [
+    { name: 'Chris Ward', title: 'Website developer and game developer', image: '/pics/member1.jpg' },
+    { name: 'Trent Nguyen', title: 'Position', image: '/pics/member1.jpg' },
+    { name: 'Thomas Cuevas', title: 'Position', image: '/pics/member1.jpg' },
+    { name: 'Duc Ngo', title: 'Position', image: '/pics/member1.jpg' }
+];
 
 const Dashboard = () => {
     console.log("Dashboard is mounting");
+
     return (
         <div className="dashboard-container">
-            <TopNavigation /> {/* Updated component name */}
+            <TopNavigation />
+            <img src="/pics/Logo.png" alt="Logo" />
             <h1>Dashboard</h1>
-            {/* The rest of your dashboard content will go here */}
             <h2>Welcome back!</h2>
+            <h2>Meet our team!</h2>
+            <div className="team-section">
+                {teamMembers.map(member => (
+                    <div key={member.name} className="team-member">
+                        <img src={member.image} alt={member.name} />
+                        <h3>{member.name}</h3>
+                        <p>{member.title}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
